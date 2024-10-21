ce8fb3fac6eccc862cce4f1cb1e33fc112558866
danylo8 <ozai.avatar@gmail.com>
2024-10-21 11:49:35 -0400

    Finished

--- Main.java
+++ Main.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
+
 public class Main
     {
         public static void main(String[] args)
         {
+            WordChecker wordChecker = new WordChecker();
 
+            wordChecker.wordList.add("an");
+            wordChecker.wordList.add("band");
+            wordChecker.wordList.add("band");
+            wordChecker.wordList.add("abandon");
+
+            boolean isWordChain= wordChecker.isWordChain();
+            System.out.println(isWordChain);
+
+            WordChecker wordCheckerTwo = new WordChecker();
+
+            wordCheckerTwo.wordList.add("to");
+            wordCheckerTwo.wordList.add("too");
+            wordCheckerTwo.wordList.add("stool");
+            wordCheckerTwo.wordList.add("tools");
+
+            boolean isWordChainTwo=wordCheckerTwo.isWordChain();
+            System.out.println(isWordChainTwo);
+
+            WordChecker wordCheckerThree = new WordChecker();
+
+            wordCheckerThree.wordList.add("catch");
+            wordCheckerThree.wordList.add("bobcat");
+            wordCheckerThree.wordList.add("catchacat");
+            wordCheckerThree.wordList.add("cat");
+            wordCheckerThree.wordList.add("at");
+
+            ArrayList<String> resultList = wordCheckerThree.createList("cat");
+            System.out.println(resultList);
         }
     }
--- WordChecker.java
+++ WordChecker.java
@@ -13,6 +13,10 @@ public class WordChecker {
     {
         for (int i=1; i< wordList.size(); i++)
         {
+
+            // contains checks if a specific part of a string is in the list
+            //int i functions as our loop variable.  It is initialized as 1 and will increment until i is no longer less than the size of the wordList instance variable.
+
             if (!wordList.get(i).contains(wordList.get(i-1)))
             {
                 return false;
@@ -20,6 +24,18 @@ public class WordChecker {
             }
         }
         return true;
+    }
+
+
+    public ArrayList<String> createList(String target) {
+        ArrayList<String> result = new ArrayList<>();
+        //colon takes each value in a list
+        for (String word : wordList) {
+            if (word.startsWith(target)) {
+                result.add(word.substring(target.length()));
+            }
+        }
+        return result;
     }
 
 }
